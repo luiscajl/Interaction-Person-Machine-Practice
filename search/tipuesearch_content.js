0b3315cc1836d0cff1ff987f83c733053368ecd7
luiscajl <luisca_jl@hotmail.com>
2017-05-10 13:59:36 +0200

    up

---
+++ search/tipuesearch_content.js
@@ -0,0 +1,12 @@
+
+var tipuesearch = {"pages": [
+     {"title": "Fin de semana de esquí en Candanchú", "text": "Te proponemos un fin de semana esquiando en parajes naturales. El viaje cuenta con 50 plazas que se completarán segun el orden de inscripción.", "tags": "Esqui Snow Nieve Deportes Jóvenes", "url": "pages/esqui.html"},
+     {"title": "Talleres de orientación profesional", "text": " Ofrecemos orientación y información para los jóvenes que finalizan los diferentes niveles educativos sobre las alternativas de formación.", "tags": "Talleres Jóvenes", "url": "event.html"},
+     {"title": "Partido de fútbol en Madrid", "text": "   Real Madrid - Barsa un partidazo clásico para disfrutar con la familia.", "tags": "Futbol Partido RealMadrid Real Madrid Barsa Barcelona Jóvenes", "url": "pages/futbol.html"},
+     {"title": "Exposición de pintura", "text": "  Una fantástica exposición de los mejores artistas nacionales e internacionales.", "tags": "Pintura Arte Cuadros Jóvenes", "url": "pages/pintura.html"},
+     {"title": "Competición de natación", "text": "  Campeonato de España de natación. Inscripciones abiertas.", "tags": "Natación Piscina Jóvenes", "url": "pages/natacion.html"},
+   
+]};
+
+
+
